47dd343913716a0599dca9f770d223a8a41a8ba9
Mitchell Foote <mcfoote321@gmail.com>
2022-09-14 00:22:41 -0400

    continue_work

--- chapter2/escapeSequences.cpp
+++ chapter2/escapeSequences.cpp
@@ -1,1 +1,16 @@
 //Solution to exercise 2.8
+#include<iostream>
+
+int main() {
+
+    std::cout << "2\tM\n";
+
+    std::string exitStr;
+    std::cout << "Press x to exit" << std::endl;
+    std::cin >> exitStr;
+
+    if(exitStr == "x") {
+        return 0;
+    }
+
+}
