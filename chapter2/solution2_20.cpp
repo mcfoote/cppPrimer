c421866085e1608f065477bdf460a77264f90b3e
Mitchell Foote <mcfoote321@gmail.com>
2022-09-14 21:54:02 -0400

    continueExercises

---
+++ chapter2/solution2_20.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+    int i = 42; // init i to 42
+    int *p1 = &i; // init *p1 to point to i address
+    *p1 = *p1 * *p1; // i = i * i
+    std::cout << i << std::endl;
+    
+    //prevent application autoexit
+    std::string exitStr;
+    std::cout << "Enter x to exit" << std::endl;
+    std::cin >> exitStr;
+
+    if(exitStr == "x") {
+        return 0;
+    }
+}
