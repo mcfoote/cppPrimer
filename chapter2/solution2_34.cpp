d7f1071fdf6e9927ef8e7a9ad9981bf65db7d2d8
Mitchell Foote <mcfoote321@gmail.com>
2022-09-20 02:37:14 -0400

    new solutions

---
+++ chapter2/solution2_34.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+int main() {
+
+    int i = 0, &r = i;
+    auto a = r;
+    
+    const int ci = i, &cr = ci;
+    auto b = ci;
+    auto c = cr;
+    auto d = &i;
+    auto e = &ci;
+
+    const auto f = ci;
+
+    auto &g = ci;
+    auto &h = 42;
+    const auto &j = 42;
+
+    std::cout << "a: " << a;
+    std::cout << "b: " << b;
+    std::cout << "c: " << c;
+    std::cout << "d: " << d;
+    std::cout << "e: " << e;
+    std::cout << "f: " << f;
+    std::cout << "g: " << g;
+
+    a = 42;
+    b = 42;
+    c = 42;
+    d = 42;
+    e = 42;
+    g = 42;
+
+    std::cout << "a: " << a;
+    std::cout << "b: " << b;
+    std::cout << "c: " << c;
+    std::cout << "d: " << d;
+    std::cout << "e: " << e;
+    std::cout << "f: " << f;
+    std::cout << "g: " << g;
+}
