e1e5422a4ed78d29be8f356343cc04d4ecccde42
Mitchell Foote <mcfoote321@gmail.com>
2022-09-24 20:22:06 -0400

    new solutions

---
+++ chapter3/soultion3_2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
+
+int main () {
+
+    string line;
+    while(getline(cin, line)) {
+        cout << line << endl;
+    }
+
+    return 0;
+
+}
